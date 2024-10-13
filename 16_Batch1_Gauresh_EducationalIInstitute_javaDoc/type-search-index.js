ddc155696c6f3fb3a870f86857f6d031108279b5
gauresh52 <138248715+gauresh52@users.noreply.github.com>
2024-10-14 00:16:17 +0530

    Add files via upload

---
+++ 16_Batch1_Gauresh_EducationalIInstitute_javaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Educationslinstitute"},{"p":"<Unnamed>","l":"TestEducationalinstitute"}];updateSearchResults();
